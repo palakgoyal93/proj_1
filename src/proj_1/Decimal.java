4bac49e8758495efe378868d943f304d79748cec
palakgoyal93 <127043834+palakgoyal93@users.noreply.github.com>
2023-03-05 18:48:30 +0100

    updated

--- src/proj_1/Decimal.java
+++ src/proj_1/Decimal.java
@@ -8,6 +8,7 @@ public class Decimal {
 	{
 		int n = (int) 2.70;
 		float m = 5.5f;
+		int x;
 		
 		BigDecimal d = new BigDecimal("922337203685477580912312312312312312312312312312");
 		System.out.println(d);
